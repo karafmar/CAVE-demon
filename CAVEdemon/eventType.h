8c33efb9c2f5d82ff6532dc2aff25bae6bf4c111
karafmar <karafmar@fel.cvut.cz>
2014-04-26 20:36:56 +0200

    new file:   CAVEdemon/eventType.h

---
+++ CAVEdemon/eventType.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   eventType.h
+ * Author: marketa
+ *
+ * Created on 26. duben 2014, 20:17
+ */
+
+#ifndef EVENTTYPE_H
+#define	EVENTTYPE_H
+
+enum class eventType {
+    NOTICE, NEW_DEVICE, FEEDBACK, DATA_UPDATE
+
+};
+
+#endif	/* EVENTTYPE_H */
+
