8c7d893b4f7f950b91c73eebdb8e08b995832c18
karafmar <karafmar@fel.cvut.cz>
2014-04-26 21:01:11 +0200

    new file:   CAVEdemon/inputType.h

---
+++ CAVEdemon/inputType.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   inputType.h
+ * Author: marketa
+ *
+ * Created on 26. duben 2014, 20:59
+ */
+
+#ifndef INPUTTYPE_H
+#define	INPUTTYPE_H
+
+enum class inputType {
+    BUTTON, REL_AXIS, ABS_AXIS
+//add trackers in future
+};
+
+#endif	/* INPUTTYPE_H */
+
